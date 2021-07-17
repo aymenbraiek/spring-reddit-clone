f3570c7f8f4fb44c707df44806e258be01ff394b
aymenbraiek <aymen.braiek@esprit.tn>
2021-07-17 20:55:06 +0100

    Intial Porject closes #1

---
+++ src/test/java/biat/learning/springredditclone/SpringRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package biat.learning.springredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
